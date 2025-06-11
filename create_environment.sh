4169cdf2e15044d8a96ad13e67eec37dfbca8c31
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-11 19:05:44 +0200

    fix for loop

--- create_environment.sh
+++ create_environment.sh
@@ -7,16 +7,23 @@ read -p "What is your name? " name
 parent_dir="submission_reminder_$name"
 mkdir -p $parent_dir/app $parent_dir/modules $parent_dir/assets $parent_dir/config
 echo "Parent directory created successfully"
+sleep 1
 
+# file_name=("reminder.sh" "functions.sh" "submissions.txt" "config.env")
 file_name=("reminder.sh" "functions.sh" "submissions.txt" "config.env")
 dir_list=("app" "modules" "assets" "config")
 
-for (( i=0; i<4; i++ ))
-do 
-    cp files/${file_name[i]} ${parent_dir}/${dir_list[i]}/${file_name[i]}
+for (( i=0 ; i < 4 ; i++ )); do 
+    echo $i
+    echo files/${file_name[i]} 
+    echo ${parent_dir}/${dir_list[i]}
+
+    # cp files/${file_name[i]} ${parent_dir}/${dir_list[i]}
 done
+
+echo "The setup files have been created successfully"
 
 cp files/startup.sh ${parent_dir}/startup.sh
 
-chmod +x -R *.sh
+chmod -R +x *.sh
 
