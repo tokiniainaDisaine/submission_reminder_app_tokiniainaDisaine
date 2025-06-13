f5d273580343dcfb78b0670a708d881face197cc
tokiniainaDisaine <t.disaine@alustudent.com>
2025-06-13 11:58:57 +0200

    add startup file

--- create_environment.sh
+++ create_environment.sh
@@ -4,26 +4,45 @@
 read -p "What is your name? " name
 
 # Creates the directory name according to the username
+echo "name=$name" > name.txt # stores the app name into an external file
 parent_dir="submission_reminder_$name"
-mkdir -p $parent_dir/app $parent_dir/modules $parent_dir/assets $parent_dir/config
-echo "Parent directory created successfully"
-sleep 1
+# export startup_dir="$parent_dir"
 
 # file_name=("reminder.sh" "functions.sh" "submissions.txt" "config.env")
-file_name=("reminder.sh" "functions.sh" "submissions.txt" "config.env")
 dir_list=("app" "modules" "assets" "config")
+file_name=("reminder.sh" "functions.sh" "submissions.txt" "config.env")
 
 for (( i=0 ; i < 4 ; i++ )); do 
-    echo $i
-    echo files/${file_name[i]} 
-    echo ${parent_dir}/${dir_list[i]}
+    mkdir -p $parent_dir/${dir_list[i]}
+done
+sleep 1
+echo "Parent directory created successfully"
+echo ""
 
-    # cp files/${file_name[i]} ${parent_dir}/${dir_list[i]}
+for (( i=0 ; i < 4 ; i++ )); do 
+    echo "Copying ${file_name[i]} to ${parent_dir}/${dir_list[i]}"
+    cp files/${file_name[i]} ${parent_dir}/${dir_list[i]}/${file_name[i]}
+
+    if [[ ${file_name[i]} == "functions.sh" || ${file_name[i]} == "reminder.sh" ]]; then
+        chmod +x ${parent_dir}/${dir_list[i]}/${file_name[i]}
+    fi
+
+    sleep 1
 done
 
+cp files/startup.sh ${parent_dir}
+
+{
+echo "Isaac, Git, not submitted"
+echo "Stephen, Shell Navigation, submitted"
+echo "Mark, Shell Basics, submitted"
+echo "Albert, Git, not submitted"
+echo "Nikola, Shell Navigation, submitted" 
+} >> $parent_dir/assets/submissions.txt
+
+
 echo "The setup files have been created successfully"
+echo ""
+sleep 1
 
-cp files/startup.sh ${parent_dir}/startup.sh
-
-chmod -R +x *.sh
 
--- files/startup.sh
+++ files/startup.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
-source ./create_environment.sh
+source ./name.txt
+parent_dir="submission_reminder_$name"
+# echo $name
 
-./${parent_dir}/reminder.sh
+# function to start the app
+start_app() {
+    echo "Engaging the starting sequence"
+    cd $parent_dir
+
+    if [ -f "./config/confing.env" ]; then
+        source ./config/confing.env
+        echo "Config loaded successfully"
+        sleep 1
+    else 
+        echo "[!] Error: encountered problem with config"
+    fi
+    
+    if [ -f "./modules/functions.sh" ]; then
+        source ./modules/functions.sh
+        echo "Modules loaded successfully"
+        sleep 1
+    else 
+        echo "[!] Error: encountered problem with modules"
+    fi
+
+    if [ -x "./app/reminder.sh" ]; then
+        source ./app/reminder.sh
+        echo "App loaded successfully"
+        sleep 1
+    else 
+        echo "[!] Error: encountered problem with app"
+        echo "Hint: Execution permission not granted"
+    fi
+}
+
+start_app
